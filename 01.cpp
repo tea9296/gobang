1f91cf3d848a7ec4103229133f7ce38273578f1c
tea9296 <tea9296@gmail.com>
2017-06-21 16:29:07 +0800

    0621
    終於完成了幹你娘

---
+++ 01.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream> 
+#include<string> 
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
+using namespace std; 
+
+int main(){
+	int k1[10]={-1,-2,-3,-4};
+	if(k1[3]) cout<<"true1\n";
+	
+	if(k1[8])  cout<<"true2\n";
+	
+	
+	return 0;
+}
